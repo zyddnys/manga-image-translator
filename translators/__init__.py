07117c78d92d97a590df25dcbb343c4e9dc7f39b
pokedexter <dol2848@naver.com>
2022-02-28 23:27:14 +0900

    add papago translator (auto key)

--- translators/__init__.py
+++ translators/__init__.py
@@ -1,6 +1,6 @@
 
 from typing import List
-from . import baidu, google, youdao, deepl
+from . import baidu, google, youdao, deepl, papago
 
 import googletrans
 
@@ -112,9 +112,31 @@ LANGUAGE_CODE_MAP['deepl'] = {
 	'TRK': 'NONE',
 }
 
+LANGUAGE_CODE_MAP['papago'] = {
+	'CHS': 'zh-CN',
+	'CHT': 'zh-TW',
+	'JPN': "ja",
+	'ENG': 'en',
+	'KOR': 'ko',
+	'VIN': 'vi',
+	'CSY': 'NONE',
+	'NLD': 'NONE',
+	'FRA': 'fr',
+	'DEU': 'de',
+	'HUN': 'NONE',
+	'ITA': 'it',
+	'PLK': 'NONE',
+	'PTB': 'pt',
+	'ROM': 'NONE',
+	'RUS': 'ru',
+	'ESP': 'es',
+	'TRK': 'NONE',
+}
+
 GOOGLE_CLIENT = google.Translator()
 BAIDU_CLIENT = baidu.Translator()
 YOUDAO_CLIENT = youdao.Translator()
+PAPAGO_CLIENT = papago.Translator()
 try:
 	DEEPL_CLIENT = deepl.Translator()
 except Exception as e:
@@ -123,7 +145,7 @@ except Exception as e:
 
 
 async def dispatch(translator: str, src_lang: str, tgt_lang: str, texts: List[str], *args, **kwargs) -> List[str] :
-	if translator not in ['google', 'youdao', 'baidu', 'deepl', 'null'] :
+	if translator not in ['google', 'youdao', 'baidu', 'deepl', 'eztrans', 'papago', 'null'] :
 		raise Exception
 	if translator == 'null' :
 		return texts
@@ -133,16 +155,19 @@ async def dispatch(translator: str, src_lang: str, tgt_lang: str, texts: List[st
 		raise Exception
 	if src_lang not in VALID_LANGUAGES and src_lang != 'auto' :
 		raise Exception
-	tgt_lang = LANGUAGE_CODE_MAP[translator][tgt_lang]
-	src_lang = LANGUAGE_CODE_MAP[translator][src_lang] if src_lang != 'auto' else 'auto'
+	if translator != 'eztrans':
+		tgt_lang = LANGUAGE_CODE_MAP[translator][tgt_lang]
+		src_lang = LANGUAGE_CODE_MAP[translator][src_lang] if src_lang != 'auto' else 'auto'
+	else:
+		tgt_lang = 'KOR'
+		src_lang = 'JPN'
 	if tgt_lang == 'NONE' or src_lang == 'NONE' :
 		raise Exception
-	TEXTBLK_BREAK = '\n###\n'
 	if translator == 'google' :
-		concat_texts = TEXTBLK_BREAK.join(texts)
+		concat_texts = "\n".join(texts)
 		result = await GOOGLE_CLIENT.translate(concat_texts, tgt_lang, src_lang, *args, **kwargs)
 		if not isinstance(result, list):
-			result = result.text.split(TEXTBLK_BREAK.replace('\n', ''))
+			result = result.text.split('\n')
 		result = [text.lstrip().rstrip() for text in result]
 	elif translator == 'baidu' :
 		concat_texts = '\n'.join(texts)
@@ -153,6 +178,9 @@ async def dispatch(translator: str, src_lang: str, tgt_lang: str, texts: List[st
 	elif translator == 'deepl' :
 		concat_texts = '\n'.join(texts)
 		result = await DEEPL_CLIENT.translate(src_lang, tgt_lang, concat_texts)
+	elif translator == 'papago' :
+		concat_texts = '\n'.join(texts)
+		result = await PAPAGO_CLIENT.translate(src_lang, tgt_lang, concat_texts)
 	translated_sentences = []
 	if len(result) < len(texts) :
 		translated_sentences.extend(result)
