fix manga2eng rotation for default detection model

text_rendering/__init__.py
@@ -171,14 +171,19 @@ async def dispatch_eng_render(img_canvas: np.ndarray, original_img: np.ndarray, 
 		blk_list = []
 		for region, tr in zip(text_regions, translated_sentences):
 			x = np.min(region.pts[:, 0])
-			w = np.max(region.pts[:, 0]) - x
+			x2 = np.max(region.pts[:, 0])
 			y = np.min(region.pts[:, 1])
-			h = np.max(region.pts[:, 1]) - y
-			font_size = region.font_size * 0.7
-			blk = TextBlock([x, y, w, h], lines=[region.pts], translation=tr, angle=region.angle, font_size=font_size)
+			y2 = np.max(region.pts[:, 1])
+			font_size = region.font_size		# default detector generate larger text polygons in my exp
+			angle = np.rad2deg(region.angle) - 90
+			if abs(angle) < 3:
+				angle = 0
+			blk = TextBlock([x, y, x2, y2], lines=[region.pts], translation=tr, angle=angle, font_size=font_size)
 			blk_list.append(blk)
 		return render_textblock_list_eng(img_canvas, blk_list, font_path, size_tol=1.2, original_img=original_img, downscale_constraint=0.5)
 	
 	for blk, tr in zip(text_regions, translated_sentences):
 		blk.translation = tr
+		
+
 	return render_textblock_list_eng(img_canvas, text_regions, font_path, size_tol=1.2, original_img=original_img, downscale_constraint=0.8)