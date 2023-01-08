f60aa5fba2ac8181506c39dd76611e9420a2e3f0
thatDudo <thatdudo@proton.me>
2023-01-09 00:59:41 +0100

    Fix SelectiveOfflineTranslator

--- translators/selective.py
+++ translators/selective.py
@@ -63,7 +63,7 @@ class SelectiveOfflineTranslator(OfflineTranslator):
                 return sugoi_translator
         return get_translator('nllb')
 
-    async def _translate(self, from_lang: str, to_lang: str, queries: List[str]) -> List[str]:
+    async def translate(self, from_lang: str, to_lang: str, queries: List[str], use_mtpe: bool) -> List[str]:
         self._real_translator = self.select_translator(from_lang, to_lang, queries)
         print(f' -- Selected translator: {self._real_translator.__class__.__name__}')
 
@@ -71,7 +71,7 @@ class SelectiveOfflineTranslator(OfflineTranslator):
             await self._real_translator.load(*self._cached_load_params)
             self._cached_load_params = None
 
-        return await self._real_translator.translate(from_lang, to_lang, queries)
+        return await self._real_translator.translate(from_lang, to_lang, queries, use_mtpe)
 
     async def load(self, from_lang: str, to_lang: str, device: str):
         self._cached_load_params = [from_lang, to_lang, device]
