7fe697bb939d9e668d350a61d18076dd4a0b4a9c
thatDudo <thatdudo@proton.me>
2023-02-10 20:47:32 +0100

    Update args

--- manga_translator/args.py
+++ manga_translator/args.py
@@ -10,13 +10,18 @@ parser.add_argument('-m', '--mode', default='demo', type=str, choices=['demo', '
 parser.add_argument('-i', '--input', default='', type=str, help='Path to an image file if using demo mode, or path to an image folder if using batch mode')
 parser.add_argument('-o', '--dest', default='', type=str, help='Path to the destination folder for translated images in batch mode')
 parser.add_argument('-l', '--target-lang', default='CHS', type=str, choices=VALID_LANGUAGES, help='Destination language')
+parser.add_argument('-v', '--verbose', action='store_true', help='Print debug info and save intermediate images in result folder')
 parser.add_argument('--detector', default='default', type=str, choices=DETECTORS, help='Text detector used for creating a text mask from an image')
 parser.add_argument('--ocr', default='48px_ctc', type=str, choices=OCRS, help='Optical character recognition (OCR) model to use')
 parser.add_argument('--inpainter', default='lama_mpe', type=str, choices=INPAINTERS, help='Inpainting model to use')
 parser.add_argument('--translator', default='google', type=str, choices=TRANSLATORS, help='Language translator to use')
 parser.add_argument('--mtpe', action='store_true', help='Turn on/off machine translation post editing (MTPE) on the command line (works only on linux right now)')
-parser.add_argument('--use-cuda', action='store_true', help='Turn on/off cuda')
-parser.add_argument('--use-cuda-limited', action='store_true', help='Turn on/off cuda (excluding offline translator)')
+
+g = parser.add_mutually_exclusive_group()
+g.add_argument('--use-cuda', action='store_true', help='Turn on/off cuda')
+g.add_argument('--use-cuda-limited', action='store_true', help='Turn on/off cuda (excluding offline translator)')
+parser.add_argument_group(g)
+
 parser.add_argument('--detection-size', default=1536, type=int, help='Size of image used for detection')
 parser.add_argument('--det-rearrange-max-batches', default=4, type=int, help='Max batch size produced by the rearrangement of image with extreme aspectio, reduce it if cuda OOM')
 parser.add_argument('--inpainting-size', default=2048, type=int, help='Size of image used for inpainting (too large will result in OOM)')
@@ -38,7 +43,6 @@ parser.add_argument('--host', default='127.0.0.1', type=str, help='Used by web m
 parser.add_argument('--port', default=5003, type=int, help='Used by web module to decide which port to attach to')
 parser.add_argument('--log-web', action='store_true', help='Used by web module to decide if web logs should be surfaced')
 parser.add_argument('--ws-url', default='ws://localhost:5000', type=str, help='Server URL for WebSocket mode')
-parser.add_argument('-v', '--verbose', action='store_true', help='Print debug info and save intermediate images')
 
 # Generares dict with a default value for each argument
 DEFAULT_ARGS = vars(parser.parse_args({}))
