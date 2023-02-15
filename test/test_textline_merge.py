fdd127d52c6a71a30ebf3c9d6df2839ccada6a0f
thatDudo <thatdudo@proton.me>
2023-02-15 21:33:57 +0100

    Add test cases to textline merge

--- test/test_textline_merge.py
+++ test/test_textline_merge.py
@@ -3,12 +3,13 @@ import sys
 import cv2
 import pytest
 from typing import List
+import numpy as np
 
 pytest_plugins = ('pytest_asyncio')
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from manga_translator.detection.textline_merge import *
+from manga_translator.detection.textline_merge import dispatch as dispatch_merge
 from manga_translator.utils import (
     TextBlock,
     Quadrilateral,
@@ -18,6 +19,23 @@ from manga_translator.utils import (
 
 BBOX_IMAGE_FOLDER = 'test/testdata/bboxes'
 os.makedirs(BBOX_IMAGE_FOLDER, exist_ok=True)
+
+async def generate_test_case(lines: List[List[List[int]]], width: int, height: int):
+    """
+    Currently only prints out generated combinations.
+    """
+    regions = await dispatch_merge([Quadrilateral(np.array(line), '', 1) for line in lines], width, height)
+    generated_combinations = []
+    for region in regions:
+        combination = []
+        for rline in region.lines:
+            for i, line in enumerate(lines):
+                if (line == rline).all():
+                    combination.append(i)
+                    break
+        combination.sort()
+        generated_combinations.append(combination)
+    print(generated_combinations)
 
 def save_regions_to_image(path: str, regions: TextBlock, width: int, height: int):
     img = np.zeros((height, width, 3))
@@ -33,7 +51,7 @@ def find_region_containing_line(line, regions):
                 return i
     raise ValueError('regions do not contain line')
 
-def extract_line_combinations(lines: List[List[int]], regions: List[TextBlock]):
+def extract_line_combinations(lines: List[List[List[int]]], regions: List[TextBlock]):
     """
     Yields region index and lines contained in the region. Lines get converted into
     indices of the `lines` argument.
@@ -52,7 +70,7 @@ def extract_line_combinations(lines: List[List[int]], regions: List[TextBlock]):
 
 async def run_test(lines, expected_combinations, width, height):
     quadrilaterals = [Quadrilateral(np.array(line), '', 1) for line in lines]
-    regions = await dispatch(quadrilaterals, width, height)
+    regions = await dispatch_merge(quadrilaterals, width, height)
 
     for i, line_combination in extract_line_combinations(lines, regions):
         if line_combination not in expected_combinations:
@@ -77,7 +95,7 @@ async def run_test(lines, expected_combinations, width, height):
 
 
 @pytest.mark.asyncio
-async def test_demo3():
+async def test_merge_image1(): # demo/image/original3.jpg
     width, height = 2590, 4096
     lines = [
         [[   0, 3280], [ 237, 3234], [ 394, 4069], [ 149, 4096]],
@@ -103,16 +121,55 @@ async def test_demo3():
         [[2269,  376], [2370,  376], [2370,  818], [2269,  818]],
         [[ 197,   42], [2405,   37], [2405,  362], [ 197,  368]],
     ]
-    expected_combinations = [
-        [0],
-        [1, 2, 3, 4],
-        [5, 6, 7],
-        [8, 9],
-        [10, 11, 12],
-        [13, 14],
-        [15],
-        [16, 17],
-        [18, 19, 20],
-        [21],
+    # await generate_test_case(lines, width, height)
+    expected_combinations = [[0], [1, 2, 3, 4], [5, 6, 7], [8, 9], [10, 11, 12], [13, 14], [15], [16, 17], [18, 19, 20], [21]]
+    await run_test(lines, expected_combinations, width, height)
+
+@pytest.mark.asyncio
+async def test_merge_image2():
+    width, height = 1317, 1637
+    lines = [
+        [[ 555, 1327], [ 609, 1311], [ 641, 1423], [ 588, 1439]],
+        [[ 588, 1297], [ 637, 1285], [ 665, 1396], [ 616, 1407]],
+        [[ 229, 1033], [ 280, 1019], [ 303, 1107], [ 252, 1121]],
+        [[ 265,  996], [ 311,  992], [ 318, 1078], [ 272, 1082]],
+        [[  65,  953], [ 111,  950], [ 149, 1434], [ 102, 1437]],
+        [[ 119,  947], [ 169,  944], [ 219, 1579], [ 169, 1582]],
+        [[1218,  894], [1271,  899], [1234, 1251], [1180, 1245]],
+        [[1156,  886], [1219,  893], [1158, 1441], [1095, 1435]],
+        [[1243,  201], [1305,  213], [1190,  800], [1128,  788]],
+        [[1181,  189], [1246,  201], [1185,  557], [1120,  545]],
+        [[1130,  180], [1190,  191], [1090,  686], [1030,  674]],
+        [[1075,  169], [1133,  181], [1025,  718], [ 966,  706]],
+        [[1009,  154], [1076,  166], [1033,  422], [ 966,  410]],
+        [[ 960,  142], [1023,  155], [ 910,  694], [ 847,  682]],
+        [[ 742,   31], [ 804,   38], [ 759,  489], [ 698,  482]],
+        [[ 688,   26], [ 744,   33], [ 669,  720], [ 612,  714]],
+        [[ 624,   14], [ 686,   21], [ 629,  573], [ 568,  566]],
+        [[ 566,    9], [ 629,   15], [ 585,  473], [ 522,  466]],
     ]
+    # await generate_test_case(lines, width, height)
+    expected_combinations = [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9, 10, 11, 12, 13], [14, 15, 16, 17]]
+    await run_test(lines, expected_combinations, width, height)
+
+@pytest.mark.asyncio
+async def test_merge_image3():
+    width, height = 1920, 1360
+    lines = [
+        [[  46,  467], [ 103,  462], [ 158, 1122], [ 101, 1127]],
+        [[1651,  322], [1703,  318], [1716,  512], [1663,  516]],
+        [[1702,  317], [1756,  315], [1778,  748], [1725,  751]],
+        [[1758,  313], [1810,  311], [1825,  638], [1773,  641]],
+        [[ 752,  261], [ 800,  265], [ 775,  525], [ 727,  521]],
+        [[ 471,  228], [ 528,  221], [ 627,  935], [ 570,  942]],
+        [[1243,  128], [1631,  101], [1688,  888], [1301,  916]],
+        [[ 540,  215], [ 597,  207], [ 681,  812], [ 623,  820]],
+        [[ 592,  181], [ 662,  166], [ 715,  412], [ 645,  427]],
+        [[ 852,  107], [ 903,  101], [ 962,  633], [ 911,  640]],
+        [[ 223,   97], [ 288,   97], [ 297,  936], [ 232,  936]],
+        [[1816,   45], [1885,   50], [1862,  345], [1793,  340]],
+        [[1745,   42], [1815,   43], [1808,  346], [1738,  345]],
+    ]
+    # await generate_test_case(lines, width, height)
+    expected_combinations = [[0], [10], [1, 2, 3], [11, 12], [5, 7, 8], [4], [9], [6]]
     await run_test(lines, expected_combinations, width, height)
