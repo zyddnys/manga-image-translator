45d2f379ec1f903b57a6eca2fd19469a7195e6ae
thatDudo <thatdudo@proton.me>
2023-03-23 22:21:08 +0100

    Add --det-invert and --det-rotate which will apply filter for detection

--- manga_translator/detection/common.py
+++ manga_translator/detection/common.py
@@ -7,85 +7,117 @@ import cv2
 from .textline_merge import dispatch as dispatch_textline_merge
 from ..utils import InfererModule, ModelWrapper, TextBlock, Quadrilateral
 
+
 class CommonDetector(InfererModule):
     async def _merge_textlines(self, textlines: List[Quadrilateral], img_width: int, img_height: int, verbose: bool = False) -> List[TextBlock]:
         return await dispatch_textline_merge(textlines, img_width, img_height, verbose)
 
-    async def detect(self, image: np.ndarray, detect_size: int, text_threshold: float, box_threshold: float,
-                     unclip_ratio: float, det_rearrange_max_batches: int, auto_orient: bool = False,
-                     verbose: bool = False) -> Tuple[List[TextBlock], np.ndarray]:
+    async def detect(self, image: np.ndarray, detect_size: int, text_threshold: float, box_threshold: float, unclip_ratio: float,
+                     invert: bool, rotate: bool, auto_rotate: bool = False, verbose: bool = False) -> Tuple[List[TextBlock], np.ndarray]:
         '''
         Returns textblock list and text mask.
         '''
-        # Add border to small-sized images (instead of simply resizing) due to them likely containing large fonts
-        bordered_image_size = detect_size // 2
-        img_h, img_w = image.shape[:2]
-        new_w, new_h = img_w, img_h
-        if min(img_w, img_h) < bordered_image_size:
-            new_w = new_h = max(img_w, img_h, bordered_image_size)
-            new_image = np.zeros([new_h, new_w, 3]).astype(np.uint8)
-            # new_image[:] = np.array([255, 255, 255], np.uint8)
-            new_image[:img_h, :img_w] = image
-            image = new_image
 
-        text_regions, raw_mask, mask = await self._detect(image, detect_size, text_threshold, box_threshold, unclip_ratio, det_rearrange_max_batches, verbose)
+        # Apply filters
+        img_h, img_w = image.shape[:2]
+        orig_image = image
+        minimum_image_size = 800
+        add_border = min(img_w, img_h) < minimum_image_size
+        # Add border if image too small (instead of simply resizing due to them likely containing large fonts)
+        if add_border:
+            self.logger.debug('Adding border')
+            image = self._add_border(image, minimum_image_size)
+        if invert:
+            self.logger.debug('Adding invertion')
+            image = self._add_invertion(image)
+        if rotate:
+            self.logger.debug('Adding rotation')
+            image = self._add_rotation(image)
+
+        cv2.imshow('', image)
+        cv2.waitKey(0)
+
+        # Run detection
+        text_regions, raw_mask, mask = await self._detect(image, detect_size, text_threshold, box_threshold, unclip_ratio, verbose)
         text_regions = self._sort_regions(text_regions, image.shape[1], image.shape[0])
 
-        # Rotate if horizontal aspect ratios are prevalent to potentially improve detection
-        if auto_orient:
+        # Remove filters
+        if rotate:
+            text_regions, raw_mask, mask = self._remove_rotation(text_regions, raw_mask, mask)
+        if auto_rotate:
+            # Rotate if horizontal aspect ratios are prevalent to optentially improve detection
             if len(text_regions) > 0:
                 orientations = ['h' if region.polygon_aspect_ratio < 90 else 'v' for region in text_regions]
                 majority_orientation = Counter(orientations).most_common(1)[0][0]
             else:
                 majority_orientation = 'h'
-
             if majority_orientation == 'h':
                 self.logger.info('Rerunning detection with 90° rotation')
-                rot_image = np.rot90(image, k=-1)
-                text_regions, raw_mask, mask = await self.detect(rot_image, detect_size, text_threshold, box_threshold, unclip_ratio, det_rearrange_max_batches, auto_orient=False)
-                
-                for i, region in enumerate(text_regions):
-                    rot_lines = region.lines[:,:,[1,0]]
-                    rot_lines[:,:,1] = -rot_lines[:,:,1] + image.shape[0]
-                    # TODO: Copy over all values
-                    new_region = TextBlock(rot_lines, font_size=region.font_size, angle=region.angle, prob=region.prob,
-                                           fg_color=region.fg_colors, bg_color=region.bg_colors)
-                    text_regions[i] = new_region
-
-                if raw_mask is not None:
-                    raw_mask = np.ascontiguousarray(np.rot90(raw_mask))
-                if mask is not None:
-                    mask = np.ascontiguousarray(np.rot90(mask).astype(np.uint8))
-                return text_regions, raw_mask, mask
-
-        # Remove border
-        if new_w > img_w or new_h > img_h:
-            if mask is not None:
-                mask = cv2.resize(mask, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
-                mask = mask[:img_h, :img_w]
-                new_text_regions = []
-            if raw_mask is not None:
-                raw_mask = cv2.resize(raw_mask, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
-                raw_mask = raw_mask[:img_h, :img_w]
-                new_text_regions = []
-            # Filter out regions within the border and clamp the points of the remaining regions
-            for region in text_regions:
-                if region.xyxy[0] >= img_w and region.xyxy[1] >= img_h:
-                    continue
-                region.xyxy[2] = min(region.xyxy[2], img_w)
-                region.xyxy[3] = min(region.xyxy[3], img_h)
-                for line in region.lines:
-                    for pt in line:
-                        pt[0] = min(pt[0], img_w)
-                        pt[1] = min(pt[1], img_h)
-                new_text_regions.append(region)
-            text_regions = new_text_regions
+                return await self.detect(orig_image, detect_size, text_threshold, box_threshold, unclip_ratio, invert, rotate=(not rotate), auto_rotate=False, verbose=verbose)
+        # if invert:
+        #     text_regions, raw_mask, mask = self._remove_invertion(text_regions, raw_mask, mask)
+        if add_border:
+            text_regions, raw_mask, mask = self._remove_border(image, img_w, img_h, text_regions, raw_mask, mask)
+            
         return text_regions, raw_mask, mask
 
     @abstractmethod
     async def _detect(self, image: np.ndarray, detect_size: int, text_threshold: float, box_threshold: float,
-                      unclip_ratio: float, det_rearrange_max_batches:int, verbose: bool = False) -> Tuple[List[TextBlock], np.ndarray]:
+                      unclip_ratio: float, verbose: bool = False) -> Tuple[List[TextBlock], np.ndarray]:
         pass
+
+    def _add_border(self, image: np.ndarray, target_side_length: int):
+        old_h, old_w = image.shape[:2]
+        new_w = new_h = max(old_w, old_h, target_side_length)
+        new_image = np.zeros([new_h, new_w, 3]).astype(np.uint8)
+        # new_image[:] = np.array([255, 255, 255], np.uint8)
+        new_image[:old_h, :old_w] = image
+        return new_image
+
+    def _remove_border(self, image: np.ndarray, old_w: int, old_h: int, text_regions, raw_mask, mask):
+        new_h, new_w = image.shape[:2]
+        raw_mask = cv2.resize(raw_mask, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
+        raw_mask = raw_mask[:old_h, :old_w]
+        if mask is not None:
+            mask = cv2.resize(mask, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
+            mask = mask[:old_h, :old_w]
+
+        # Filter out regions within the border and clamp the points of the remaining regions
+        new_text_regions = []
+        for region in text_regions:
+            if region.xyxy[0] >= old_w and region.xyxy[1] >= old_h:
+                continue
+            region.xyxy[2] = min(region.xyxy[2], old_w)
+            region.xyxy[3] = min(region.xyxy[3], old_h)
+            for line in region.lines:
+                for pt in line:
+                    pt[0] = min(pt[0], old_w)
+                    pt[1] = min(pt[1], old_h)
+            new_text_regions.append(region)
+        return new_text_regions, raw_mask, mask
+        
+    def _add_rotation(self, image: np.ndarray):
+        return np.rot90(image, k=-1)
+    
+    def _remove_rotation(self, text_regions, raw_mask, mask):
+        raw_mask = np.ascontiguousarray(np.rot90(raw_mask))
+        if mask is not None:
+            mask = np.ascontiguousarray(np.rot90(mask).astype(np.uint8))
+
+        for i, region in enumerate(text_regions):
+            rot_lines = region.lines[:,:,[1,0]]
+            rot_lines[:,:,1] = -rot_lines[:,:,1] + raw_mask.shape[0]
+            # TODO: Copy over all values
+            new_region = TextBlock(rot_lines, font_size=region.font_size, angle=region.angle, prob=region.prob,
+                                   fg_color=region.fg_colors, bg_color=region.bg_colors)
+            text_regions[i] = new_region
+        return text_regions, raw_mask, mask
+
+    def _add_invertion(self, image: np.ndarray):
+        return cv2.bitwise_not(image)
+
+    # def _remove_invertion(self, text_regions, raw_mask, mask):
+    #     pass
 
     def _sort_regions(self, regions: List[TextBlock], width: int, height: int) -> List[TextBlock]:
         # Sort regions from right to left, top to bottom
@@ -113,5 +145,5 @@ class OfflineDetector(CommonDetector, ModelWrapper):
 
     @abstractmethod
     async def _infer(self, image: np.ndarray, detect_size: int, text_threshold: float, box_threshold: float,
-                       unclip_ratio: float, det_rearrange_max_batches: int, verbose: bool = False) -> Tuple[List[TextBlock], np.ndarray]:
+                       unclip_ratio: float, verbose: bool = False) -> Tuple[List[TextBlock], np.ndarray]:
         pass
--- manga_translator/manga_translator.py
+++ manga_translator/manga_translator.py
@@ -355,8 +355,7 @@ class MangaTranslator():
 
     async def _run_detection(self, ctx: Context):
         return await dispatch_detection(ctx.detector, ctx.img_rgb, ctx.detection_size, ctx.text_threshold, ctx.box_threshold,
-                                        ctx.unclip_ratio, ctx.det_rearrange_max_batches, ctx.detection_auto_orient,
-                                        self.device, self.verbose)
+                                        ctx.unclip_ratio, ctx.det_invert, ctx.det_rotate, ctx.det_auto_rotate, self.device, self.verbose)
 
     async def _run_ocr(self, ctx: Context):
         text_regions = await dispatch_ocr(ctx.ocr, ctx.img_rgb, ctx.text_regions, self.device, self.verbose)
