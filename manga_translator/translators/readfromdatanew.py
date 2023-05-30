fb7a1b9a3f6040a58b13ac5691b98f0b2d658ff6
1439707509 <128567416+1439707509@users.noreply.github.com>
2023-05-31 02:00:46 +0800

    Update readfromdatanew.py

--- manga_translator/translators/readfromdatanew.py
+++ manga_translator/translators/readfromdatanew.py
@@ -48,38 +48,38 @@ class ReadTextTranslator(CommonTranslator):
 		cursor.execute("SELECT trans FROM manga_page ORDER BY id ")
 		#result = c.fetchone()
 		result = cursor.fetchall()
-		# 将trans字段值通过\r\n分割成translations数组
+		# Split the trans field value into a translations array by \r\n
 		# translations = result[page - 1][0].split('\r\n')
 		translations = result[page - 1][0].split('\n')
 
 		result_list = []
 
-		# 使用列表解析和strip()去除字符串的首尾的空格和制表符
+		# Use list comprehension and strip() to remove leading and trailing spaces and tabs from strings
 		translations = [s.strip() for s in translations]
 
 		result_list.extend(translations)
 
-		# 如果翻译长度不够，补充占位
+		# If the translation length is not enough, add placeholders
 		if len(result_list) < len(queries):
 			result_list += [''] * (len(queries) - len(result_list))
-		# 如果长度过长，拼接在末尾（实际效果待测试）
+		# If the length is too long, splicing at the end (actual effect to be tested)
 		if len(result_list) > len(queries):
 			new_end = ''.join(result_list[len(queries):])
 			result_list = result_list[:len(queries)]
 			last_string = result_list[-1] + new_end
 			result_list[-1] = last_string
 
-		# 创建表格
+		# create form
 		#cursor.execute('''CREATE TABLE manga_page(id INTEGER PRIMARY KEY AUTOINCREMENT, words TEXT, trans TEXT)''')
 
-		# 更新page字段值
+		# Update page field value
 		new_page = page + 1
 		cursor.execute('UPDATE page_count SET page=? WHERE rowid=1', (new_page,))
 
-		# 提交更改
+		# commit changes
 		conn.commit()
 
-		# 关闭连接
+		# close connection
 		conn.close()
 		return result_list
 
