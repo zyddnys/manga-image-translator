ce58730a62157c27d26145e7e4789165c278229e
vaibhavb02 <33981696+vaibhavb02@users.noreply.github.com>
2023-06-21 10:56:15 +0530

    Update save.py
    wrapped the code and made it easy for try/except

--- manga_translator/save.py
+++ manga_translator/save.py
@@ -9,21 +9,17 @@ class FormatNotSupportedException(Exception):
     def __init__(self, fmt: str):
         super().__init__(f'Format {fmt} is not supported.')
 
-OUTPUT_FORMATS = {}
-def register_format(format_cls):
-    for fmt in format_cls.SUPPORTED_FORMATS:
-        if fmt in OUTPUT_FORMATS:
-            raise Exception(f'Tried to register multiple ExportFormats for "{fmt}"')
-        OUTPUT_FORMATS[fmt] = format_cls()
-    return format_cls
 
-class ExportFormat():
+class ExportFormat(metaclass=ABCMeta):
     SUPPORTED_FORMATS = []
 
     # Subclasses will be auto registered
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        register_format(cls)
+        for fmt in cls.SUPPORTED_FORMATS:
+            if fmt in OUTPUT_FORMATS:
+                raise Exception(f'Tried to register multiple ExportFormats for "{fmt}"')
+            OUTPUT_FORMATS[fmt] = cls
 
     def save(self, result: Image.Image, dest: str, ctx: Context):
         self._save(result, dest, ctx)
@@ -31,6 +27,7 @@ class ExportFormat():
     @abstractmethod
     def _save(self, result: Image.Image, dest: str, ctx: Context):
         pass
+
 
 def save_result(result: Image.Image, dest: str, ctx: Context):
     _, ext = os.path.splitext(dest)
@@ -44,12 +41,15 @@ def save_result(result: Image.Image, dest: str, ctx: Context):
 
 # -- Format Implementations
 
+@register_format
 class ImageFormat(ExportFormat):
     SUPPORTED_FORMATS = ['png', 'webp']
 
     def _save(self, result: Image.Image, dest: str, ctx: Context):
         result.save(dest)
 
+
+@register_format
 class JPGFormat(ExportFormat):
     SUPPORTED_FORMATS = ['jpg']
 
@@ -57,6 +57,7 @@ class JPGFormat(ExportFormat):
         result = result.convert('RGB')
         # Certain versions of PIL only support JPEG but not JPG
         result.save(dest, quality=ctx.save_quality, format='JPEG')
+
 
 # class KraFormat(ExportFormat):
 #     SUPPORTED_FORMATS = ['kra']
