dafcaba184a245a3224026d78dcf51a23f38f80b
vaibhavb02 <33981696+vaibhavb02@users.noreply.github.com>
2023-06-21 11:03:47 +0530

    Update __main__.py

--- manga_translator/__main__.py
+++ manga_translator/__main__.py
@@ -23,6 +23,7 @@ from .utils import (
 
 async def dispatch(args: Namespace):
     args_dict = vars(args)
+    logger = get_logger(args.mode)
 
     logger.info(f'Running in {args.mode} mode')
 
@@ -34,9 +35,9 @@ async def dispatch(args: Namespace):
             if len(args.input) != 1 or not os.path.isfile(args.input[0]):
                 raise FileNotFoundError(f'Invalid single image file path for demo mode: "{" ".join(args.input)}". Use `-m batch`.')
             dest = os.path.join(BASE_PATH, 'result/final.png')
-            args.overwrite = True # Do overwrite result/final.png file
+            args.overwrite = True  # Do overwrite result/final.png file
             await translator.translate_path(args.input[0], dest, args_dict)
-        else: # batch
+        else:  # batch
             dest = args.dest
             args.overwrite = False
             for path in natural_sort(args.input):
@@ -44,19 +45,23 @@ async def dispatch(args: Namespace):
 
     elif args.mode == 'web':
         from .server.web_main import dispatch
-        await dispatch(args.host, args.port, translation_params=args_dict)
+        loop = asyncio.get_event_loop()
+        await loop.run_until_complete(dispatch(args.host, args.port, translation_params=args_dict))
 
     elif args.mode == 'web_client':
         translator = MangaTranslatorWeb(args_dict)
-        await translator.listen(args_dict)
+        loop = asyncio.get_event_loop()
+        await loop.run_until_complete(translator.listen(args_dict))
 
     elif args.mode == 'ws':
         translator = MangaTranslatorWS(args_dict)
-        await translator.listen(args_dict)
+        loop = asyncio.get_event_loop()
+        await loop.run_until_complete(translator.listen(args_dict))
 
     elif args.mode == 'api':
         translator = MangaTranslatorAPI(args_dict)
-        await translator.listen(args_dict)
+        loop = asyncio.get_event_loop()
+        await loop.run_until_complete(translator.listen(args_dict))
 
 if __name__ == '__main__':
     args = None
