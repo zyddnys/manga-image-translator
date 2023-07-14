1537be7d2e934f4426b654e6a3d69e94f62fb576
QiroNT <i@ntz.im>
2023-07-15 05:56:55 +0800

    `cfnext` WS mode update

--- manga_translator/manga_translator.py
+++ manga_translator/manga_translator.py
@@ -12,7 +12,7 @@ import time
 import logging
 import numpy as np
 from PIL import Image
-from typing import List
+from typing import List, Tuple
 from aiohttp import web
 from marshmallow import Schema, fields, ValidationError
 
@@ -676,90 +676,196 @@ class MangaTranslatorWeb(MangaTranslator):
 
 class MangaTranslatorWS(MangaTranslator):
 
+
     def __init__(self, params: dict = None):
         super().__init__(params)
         self.url = params.get('ws_url')
         self.secret = params.get('ws_secret', os.getenv('WS_SECRET', ''))
         self.ignore_errors = params.get('ignore_errors', True)
         self._task_id = None
+        self._websocket = None
 
     async def listen(self, translation_params: dict = None):
+        from threading import Thread
         import io
-        import shutil
+        import aioshutil
+        import aiofiles
         import websockets
-
         from .server import ws_pb2
 
+        self._server_loop = asyncio.new_event_loop()
+
+        async def send_and_yield(websocket, msg):
+            # send message and yield control to the event loop (to actually send the message)
+            await websocket.send(msg)
+            await asyncio.sleep(0)
+
         async def sync_state(state, finished):
+            if self._websocket is None:
+                return
             msg = ws_pb2.WebSocketMessage()
             msg.status.id = self._task_id
             msg.status.status = state
-            await websocket.send(msg.SerializeToString())
+            self._server_loop.call_soon_threadsafe(
+                asyncio.create_task,
+                send_and_yield(self._websocket, msg.SerializeToString())
+            )
 
         self.add_progress_hook(sync_state)
 
-        async for websocket in websockets.connect(self.url, extra_headers={'x-secret': self.secret}, max_size=100_000_000):
-            try:
-                logger.info('Connected to websocket server')
+        task_lock = asyncio.Lock()
+        async def translate(task_id, websocket, image, params):
+            async with task_lock:
+                self._task_id = task_id
+                self._websocket = websocket
+                result = await self.translate(image, params)
+                self._task_id = None
+                self._websocket = None
+            return result
+        
+        async def server_send_status(websocket, task_id, status):
+            msg = ws_pb2.WebSocketMessage()
+            msg.status.id = task_id
+            msg.status.status = status
+            await websocket.send(msg.SerializeToString())
+            await asyncio.sleep(0)
 
-                async for raw in websocket:
+        async def server_process_inner(main_loop, logger_task, session, websocket, task) -> Tuple[bool, bool]:
+            logger_task.info(f'-- Processing task {task.id}')
+            await server_send_status(websocket, task.id, 'pending')
+
+            if self.verbose:
+                await aioshutil.rmtree(f'result/{task.id}', ignore_errors=True)
+                await aiofiles.os.makedirs(f'result/{task.id}', exist_ok=True)
+
+            params = {
+                'target_lang': task.target_language,
+                'detector': task.detector,
+                'direction': task.direction,
+                'translator': task.translator,
+                'size': task.size,
+            }
+
+            logger_task.info(f'-- Downloading image from {task.source_image}')
+            await server_send_status(websocket, task.id, 'downloading')
+            async with session.get(task.source_image) as resp:
+                if resp.status == 200:
+                    source_image = await resp.read()
+                else:
                     msg = ws_pb2.WebSocketMessage()
-                    msg.ParseFromString(raw)
-                    if msg.WhichOneof('message') == 'new_task':
-                        task = msg.new_task
-                        self._task_id = task.id
+                    msg.status.id = task.id
+                    msg.status.status = 'error-download'
+                    await websocket.send(msg.SerializeToString())
+                    await asyncio.sleep(0)
+                    return False, False
 
-                        if self.verbose:
-                            shutil.rmtree(f'result/{self._task_id}', ignore_errors=True)
-                            os.makedirs(f'result/{self._task_id}', exist_ok=True)
+            logger_task.info(f'-- Translating image')
+            if translation_params:
+                for p, default_value in translation_params.items():
+                    current_value = params.get(p)
+                    params[p] = current_value if current_value is not None else default_value
 
-                        params = {
-                            'target_lang': task.target_language,
-                            'detector': task.detector,
-                            'direction': task.direction,
-                            'translator': task.translator,
-                            'size': task.size,
-                        }
+            image = Image.open(io.BytesIO(source_image))
 
-                        logger.info(f'-- Processing task {self._task_id}')
-                        if translation_params:
-                            for p, default_value in translation_params.items():
-                                current_value = params.get(p)
-                                params[p] = current_value if current_value is not None else default_value
-                        image = Image.open(io.BytesIO(task.source_image))
-                        (ori_w, ori_h) = image.size
-                        if max(ori_h, ori_w) > 1200 :
-                            params['upscale_ratio'] = 1
-                        translation_dict = await self.translate(image, params)
-                        output: Image.Image = translation_dict.result
-                        if output is None:
-                            output = Image.fromarray(np.zeros((ori_h, ori_w, 4), dtype=np.uint8))
-                        else :
-                            output = output.resize((ori_w, ori_h), resample = Image.BICUBIC)
+            (ori_w, ori_h) = image.size
+            if max(ori_h, ori_w) > 1200 :
+                params['upscale_ratio'] = 1
 
-                        img = io.BytesIO()
-                        output.save(img, format='PNG')
-                        if self.verbose:
-                            output.save(self._result_path('ws_final.png'))
+            await server_send_status(websocket, task.id, 'preparing')
+            # translation_dict = await self.translate(image, params)
+            translation_dict = await asyncio.wrap_future(
+                asyncio.run_coroutine_threadsafe(
+                    translate(task.id, websocket, image, params),
+                    main_loop
+                )
+            )
+            
+            output: Image.Image = translation_dict.result
+            if output is not None:
+                await server_send_status(websocket, task.id, 'saving')
 
-                        img_bytes = img.getvalue()
+                output = output.resize((ori_w, ori_h), resample = Image.LANCZOS)
 
-                        result = ws_pb2.WebSocketMessage()
-                        result.finish_task.id = self._task_id
-                        result.finish_task.translation_mask = img_bytes
-                        await websocket.send(result.SerializeToString())
+                img = io.BytesIO()
+                output.save(img, format='PNG')
+                if self.verbose:
+                    output.save(self._result_path('ws_final.png'))
 
-                        logger.info('Waiting for translation tasks')
-                        self._task_id = None
+                img_bytes = img.getvalue()
+                logger_task.info(f'-- Uploading result to {task.translation_mask}')
+                await server_send_status(websocket, task.id, 'uploading')
+                async with session.put(task.translation_mask, data=img_bytes) as resp:
+                    if resp.status != 200:
+                        logger_task.error(f'-- Failed to upload result:')
+                        logger_task.error(f'{resp.status}: {resp.reason}')
+                        msg = ws_pb2.WebSocketMessage()
+                        msg.status.id = task.id
+                        msg.status.status = 'error-upload'
+                        await websocket.send(msg.SerializeToString())
+                        await asyncio.sleep(0)
+                        return False, False
 
+            return True, output is not None
+
+        async def server_process(main_loop, session, websocket, task) -> bool:
+            logger_task = logger.getChild(f'{task.id}')
+            try:
+                (success, has_translation_mask) = await server_process_inner(main_loop, logger_task, session, websocket, task)
             except Exception as e:
-                logger.error(f'{e.__class__.__name__}: {e}', exc_info=e if self.verbose else None)
+                logger_task.error(f'-- Task failed with exception:')
+                logger_task.error(f'{e.__class__.__name__}: {e}', exc_info=e if self.verbose else None)
+                (success, has_translation_mask) = False, False
+            finally:
+                result = ws_pb2.WebSocketMessage()
+                result.finish_task.id = task.id
+                result.finish_task.success = success
+                result.finish_task.has_translation_mask = has_translation_mask
+                await websocket.send(result.SerializeToString())
+                await asyncio.sleep(0)
+                logger_task.info(f'-- Task finished')
+
+        async def async_server_thread(main_loop):
+            from aiohttp import ClientSession
+            async with ClientSession() as session:
+                logger_conn = logger.getChild('connection')
+                logger_conn.setLevel(logging.DEBUG)
+                async for websocket in websockets.connect(
+                    self.url,
+                    extra_headers={'x-secret': self.secret},
+                    max_size=1_000_000,
+                    logger=logger_conn
+                ):
+                    try:
+                        logger.info('-- Connected to websocket server')
+
+                        async for raw in websocket:
+                            # logger.info(f'Got message: {raw}')
+                            msg = ws_pb2.WebSocketMessage()
+                            msg.ParseFromString(raw)
+                            if msg.WhichOneof('message') == 'new_task':
+                                task = msg.new_task
+                                asyncio.create_task(server_process(main_loop, session, websocket, task))
+
+                    except Exception as e:
+                        logger.error(f'{e.__class__.__name__}: {e}', exc_info=e if self.verbose else None)
+
+        def server_thread(future, main_loop, server_loop):
+            asyncio.set_event_loop(server_loop)
+            server_loop.run_until_complete(async_server_thread(main_loop))
+            future.set_result(None)
+
+        future = asyncio.Future()
+        Thread(
+            target=server_thread,
+            args=(future, asyncio.get_running_loop(), self._server_loop),
+            daemon=True
+        ).start()
+
+        # create a future that is never done
+        await future
 
     async def _run_text_rendering(self, ctx: Context):
-        render_mask = np.copy(ctx.mask)
-        render_mask[render_mask < 127] = 0
-        render_mask[render_mask >= 127] = 1
-        render_mask = render_mask[:, :, None]
+        render_mask = np.where(ctx.mask < 127, 0, 1)[:, :, None]
 
         output = await super()._run_text_rendering(ctx)
         render_mask[np.sum(ctx.img_rgb != output, axis=2) > 0] = 1
