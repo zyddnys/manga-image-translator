c31a116a4fd27721df9b838104e269fffbafe84e
My12123 <49092250+My12123@users.noreply.github.com>
2023-07-24 11:25:30 +0300

    Update Installer for Windows CUDA.bat

--- Installer for Windows CUDA.bat
+++ Installer for Windows CUDA.bat
@@ -7,4 +7,3 @@ conda activate manga-image-translator
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
 conda install -c conda-forge pydensecrf
-pause
