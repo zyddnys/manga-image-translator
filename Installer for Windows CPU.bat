e6dc6907b7279bfba08cecfe9d6193f24d579ae3
My12123 <49092250+My12123@users.noreply.github.com>
2023-07-25 19:44:56 +0300

    Update Installer for Windows CPU.bat

--- Installer for Windows CPU.bat
+++ Installer for Windows CPU.bat
@@ -2,8 +2,8 @@
 @rem Change the path to the activate.bat file
 F:
 call F:\1\Scripts\activate.bat
-conda create -n manga-image-translator python=3.10
+conda create -n manga-image-translator python=3.10 -y
 conda activate manga-image-translator
 pip3 install torch torchvision torchaudio
 pip install -r requirements.txt
-conda install -c conda-forge pydensecrf
+conda install -c conda-forge pydensecrf -y
