3caf51f65aeac8855e8025de2d3d0768ba01d774
PiDanShouRouZhouXD <38401147+PiDanShouRouZhouXD@users.noreply.github.com>
2024-02-26 23:29:49 +0800

    Remove SakuraTranslator from chatgpt imports

--- manga_translator/translators/__init__.py
+++ manga_translator/translators/__init__.py
@@ -7,7 +7,7 @@ from .youdao import YoudaoTranslator
 from .deepl import DeeplTranslator
 from .papago import PapagoTranslator
 from .caiyun import CaiyunTranslator
-from .chatgpt import GPT3Translator, GPT35TurboTranslator, GPT4Translator, SakuraTranslator
+from .chatgpt import GPT3Translator, GPT35TurboTranslator, GPT4Translator
 from .nllb import NLLBTranslator, NLLBBigTranslator
 from .sugoi import JparacrawlTranslator, JparacrawlBigTranslator, SugoiTranslator
 from .m2m100 import M2M100Translator, M2M100BigTranslator
