99a23d546bbfaaf38fc62bd3936d534a2ac5eb41
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-16 02:38:19 -0400

    remove hardcoded port

--- manga_translator/__main__.py
+++ manga_translator/__main__.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
         # setup the ngrok tunnel
         if args.ngrok:
             logger.info('Setting up ngrok tunnel...')
-            ngrok_url = ngrok.connect(5003).url()
+            ngrok_url = ngrok.connect(args.port).url()
             logger.info(f'ngrok tunnel: {ngrok_url}')
         
         loop.run_until_complete(dispatch(args))
