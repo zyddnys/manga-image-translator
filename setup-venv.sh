ed3a031cec955eb20f1e147331e1b182c1ca0a4a
Wang Guan <momocraft@gmail.com>
2024-07-17 01:10:46 +0900

    wip

--- setup-venv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -uex
-cd $(dirname "$0")
-micromamba env create -f conda.yaml || true
-
-if [[ ! -d venv ]]; then
-  micromamba run -n mit-py311 python3 -mvenv --system-site-packages ./venv
-fi
