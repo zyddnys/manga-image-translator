aef0a275d83177732f0e2f4e29db9278a18c435b
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2024-11-21 17:55:54 +0100

    changed queue a bit

--- server/myqueue.py
+++ server/myqueue.py
@@ -1,9 +1,8 @@
 import asyncio
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 from fastapi import HTTPException
 from starlette.requests import Request
-from starlette.responses import StreamingResponse
 
 from manga_translator import Context
 from server.instance import executor_instances
@@ -17,16 +16,19 @@ class TaskQueue:
     def add_task(self, task):
         self.queue.append(task)
 
-    def get_pos(self, task):
-        return self.queue.index(task)
-
-    def update_event(self):
+    def get_pos(self, task) -> Optional[int]:
+        try:
+            return self.queue.index(task)
+        except ValueError:
+            return None
+    async def update_event(self):
+        self.queue = [ctx for ctx in self.queue if not await is_client_disconnected(ctx.req)]
         self.queue_event.set()
         self.queue_event.clear()
 
-    def remove(self, task):
+    async def remove(self, task):
         self.queue.remove(task)
-        self.update_event()
+        await self.update_event()
 
     async def wait_for_event(self):
         await self.queue_event.wait()
@@ -42,18 +44,23 @@ async def wait_in_queue(task, notify: NotifyType):
     """Will get task position report it. If its in the range of translators then it will try to aquire an instance(blockig) and sent a task to it. when done the item will be removed from the queue and result will be returned"""
     while True:
         queue_pos = task_queue.get_pos(task)
+        if not queue_pos:
+            if notify:
+                return
+            else:
+                raise HTTPException(500, detail="User is no longer connected")  # just for the logs
         if notify:
             notify(3, str(queue_pos).encode('utf-8'))
         if queue_pos < executor_instances.free_executors():
             if await is_client_disconnected(task.req):
-                task_queue.remove(task)
-                task_queue.update_event()
+                await task_queue.update_event()
                 if notify:
                     return
                 else:
                     raise HTTPException(500, detail="User is no longer connected") #just for the logs
+
             instance = await executor_instances.find_executor()
-            task_queue.remove(task)
+            await task_queue.remove(task)
             if notify:
                 notify(4, b"")
             if notify:
@@ -61,19 +68,11 @@ async def wait_in_queue(task, notify: NotifyType):
             else:
                 result = await instance.sent(task.image, task.config)
 
-            executor_instances.free_executor(instance)
-            task_queue.update_event()
+            await executor_instances.free_executor(instance)
 
             if notify:
                 return
             else:
                 return result
         else:
-            if await is_client_disconnected(task.req):
-                task_queue.remove(task)
-                task_queue.update_event()
-                if notify:
-                    return
-                else:
-                    raise HTTPException(500, detail="User is no longer connected") #just for the logs
             await task_queue.wait_for_event()
