bc48eaf308eada79f0111056a4395507ed63f8f7
Faizan Khan <faizan@ismanga.com>
2025-04-29 07:37:49 -0400

    feat: added switch detection

--- front/app/config.ts
+++ front/app/config.ts
@@ -35,6 +35,7 @@ export const textDetectorOptions = [
   { value: "default", label: "Default" },
   { value: "ctd", label: "CTD" },
   { value: "paddle", label: "Paddle" },
+  { value: "switch", label: "Switch" },
 ];
 
 export const inpainterOptions = [
