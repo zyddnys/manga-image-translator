2412b7796c18b0a45393641af35354d37784db8a
zyddnys <zyddnys@outlook.com>
2025-06-23 22:41:02 -0400

    Revert "fix: minor bugs fix"

--- manga_translator/translators/config_gpt.py
+++ manga_translator/translators/config_gpt.py
@@ -55,8 +55,7 @@ class ConfigGPT:
         '- Translate line by line, maintaining accuracy and the authentic; Faithfully reproducing the original text and emotional intent.\n'          
         '- Preserve original gibberish or sound effects without translation.\n'            
         '- Output each segment with its prefix (<|number|> format exactly).\n'  
-        '- Translate content only—no additional interpretation or commentary.\n'
-        '- Do not include any explanations, analysis, or commentary on the original text or the translation.\n'  
+        '- Translate content only—no additional interpretation or commentary.\n'  
         
         'Translate the following text into {to_lang}:\n'  
     )
