fca779eebb32cc50e54ca1e72398652c30c144d4
Faizan Khan <faizan@ismanga.com>
2026-02-10 06:55:48 +0000

    refactor: change retry to 2

--- manga_translator/translators/gemini.py
+++ manga_translator/translators/gemini.py
@@ -21,9 +21,9 @@ class GeminiTranslator(CommonGPTTranslator):
     _INVALID_REPEAT_COUNT = 0  # 现在这个参数没意义了
     _MAX_REQUESTS_PER_MINUTE = 9999  # 无RPM限制
     _TIMEOUT = 10  # 在重试之前等待服务器响应的时间（秒）
-    _RETRY_ATTEMPTS = 3  # 在放弃之前重试错误请求的次数
-    _TIMEOUT_RETRY_ATTEMPTS = 3  # 在放弃之前重试超时请求的次数
-    _RATELIMIT_RETRY_ATTEMPTS = 3  # 在放弃之前重试速率限制请求的次数
+    _RETRY_ATTEMPTS = 2  # 在放弃之前重试错误请求的次数
+    _TIMEOUT_RETRY_ATTEMPTS = 2  # 在放弃之前重试超时请求的次数
+    _RATELIMIT_RETRY_ATTEMPTS = 2  # 在放弃之前重试速率限制请求的次数
 
     # 最大令牌数量，用于控制处理的文本长度
     # Maximum token count for controlling the length of text processed
